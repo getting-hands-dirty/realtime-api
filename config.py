bae18c1427300c00a5d9739d8eb10feb552165e3
Bhavaneetharan Loganathan <bhavanl@techprovint.com>
2025-01-27 19:55:41 +0530

    fix

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ import os
 
 REALTIME_AUDIO_API_URL = os.getenv(
     "REALTIME_AUDIO_API_URL",
-    "wss://api.openai.com/v1/realtime?model=gpt-4o-mini-realtime-preview",
+    "wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-12-17",
 )
+
+print(REALTIME_AUDIO_API_URL)

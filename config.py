20934886441d6c253041f1645e1014dd3e09f31d
Sandeesh Croos <sandeeshc@techprovint.com>
2025-01-27 19:03:55 +0530

    Add gpt 4 o mini

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 REALTIME_AUDIO_API_URL = (
-    "wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-12-17"
+    "wss://api.openai.com/v1/realtime?model=gpt-4o-mini-realtime-preview"
 )

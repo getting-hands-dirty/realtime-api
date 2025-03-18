c32550a9527271633ba60017729b6788e497b69e
ishjay <ishsuranga@gmail.com>
2025-03-18 22:00:34 +0530

    testing 4 config.py

--- usecases/api/config.py
+++ usecases/api/config.py
@@ -12,7 +12,7 @@ ADVANCED_SETTINGS = {
 
 # Entry message spoken out to the end user by Twilio.
 INTRO_TEXT = (
-    """Ishaaann"""
+    """Ishaaann oya koheda???? """
 )
 
 # Greeting message spoken out to the end user by AI setup.
